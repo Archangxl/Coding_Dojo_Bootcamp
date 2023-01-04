c6dc5ee58f6f9288fd036a8afa8c2ea63324468b
Alexander Mueller <101312895+Archangxl@users.noreply.github.com>
2023-01-03 17:14:19 -0700

    Update mysqlconnection.py

--- Back_end/Python/Flask_mysql/Sharing_Recipes/flask_app/config/mysqlconnection.py
+++ Back_end/Python/Flask_mysql/Sharing_Recipes/flask_app/config/mysqlconnection.py
@@ -5,7 +5,7 @@ class MySQLConnection:
 
         connection = pymysql.connect(host = 'localhost',
             user = 'root',
-            password = 'root',
+            password = '',
             db = db,
             charset = 'utf8mb4',
             cursorclass = pymysql.cursors.DictCursor,
