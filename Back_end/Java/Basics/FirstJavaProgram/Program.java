f62ea8a560228eef6a73165f3e516026d2858f99
Archangxl <lixo.mueller@gmail.com>
2023-01-07 21:12:47 -0700

    last updated 01.07.2023

---
+++ Back_end/Java/Basics/FirstJavaProgram/Program.java
@@ -0,0 +1,9 @@
+package Back_end.Java.Basics.FirstJavaProgram;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("My Name is Alexander Mueller.");
+        System.out.println("I am 22 years old.");
+        System.out.println("My hometown is Colorado Springs.");
+    }
+}
