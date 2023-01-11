5b46508224328fb0f173570aa882ad8932c24633
Alexander <lixo.mueller@gmail.com>
2023-01-11 08:13:11 -0700

    last updated 01.11.2023

---
+++ Back_end/Java/Basics/TryAndCatchExample/DeliverMessage.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+class OutOfGasException extends Exception{}
+class UnreliableFriend {
+    public boolean deliverMessage() throws OutOfGasException{
+        Random r = new Random();
+        boolean hasGas = r.nextBoolean();
+        if (hasGas){
+            return true;
+        }
+        throw new OutOfGasException();
+    }
+}
+
+class DeliverMessage{
+    public static void main(String[] args){
+        UnreliableFriend friend = new UnreliableFriend();
+        try{
+            friend.deliverMessage();
+            System.out.println("The message was delivered!");
+        } catch (OutOfGasException e){
+            System.out.println("Hey, uh, so, I ran out of gas..");
+            // back-up plan here.
+        }
+    }
+}
+
+
