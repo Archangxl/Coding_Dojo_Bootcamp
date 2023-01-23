ebf6f1bae080ac253ed1e06656dfecbad180355a
Alexander <lixo.mueller@gmail.com>
2023-01-23 15:37:30 -0700

    last updated 01.23.2023

--- Back_end/Java/Spring/DaikichiRoutes/src/main/java/com/alex/DaikichiRoutes/DaikichiController.java
+++ Back_end/Java/Spring/DaikichiRoutes/src/main/java/com/alex/DaikichiRoutes/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.alex.DaikichiRoutes;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,19 @@ public class DaikichiController {
 	@GetMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@GetMapping("/travel/{city}") 
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@GetMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
