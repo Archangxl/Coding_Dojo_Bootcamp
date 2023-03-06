968d0336109121a3f9a74ef03754ef42dbc90f0d
Alexander <lixo.mueller@gmail.com>
2023-03-06 08:50:19 -0700

    last updated 03.06.2023

---
+++ Back_end/Mern/React/First_JSX/first_jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </header>
+    </>
+  );
+}
+
+export default App;
