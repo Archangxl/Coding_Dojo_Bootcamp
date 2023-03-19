9fb71c57177ba4618b03f4abd640a504e6365084
Alexander <lixo.mueller@gmail.com>
2023-03-18 18:29:33 -0600

    last updated 03.18.2023

--- Back_end/Mern/React/more-forms/src/App.js
+++ Back_end/Mern/React/more-forms/src/App.js
@@ -5,100 +5,57 @@ import React, {useState} from 'react';
 function App() {
 
   const [firstName, setFirstName] = useState("");
-  const [firstNameError, setFirstNameError] = useState("");
-
-  const firstNameValidation = (e) => {
-    setFirstName(e.target.value);
-    if (firstName.length < 2) {
-      return setFirstNameError("First Name must be at least 2 characters!");
-    } else {
-      return setFirstNameError("");
-    }
-  }
-
   const [lastName, setLastName] = useState("");
-  const [lastNameError, setLastNameError] = useState("");
-
-  const lastNameValidation = (e) => {
-    setLastName(e.target.value);
-    if (lastName.length < 2) {
-      return setLastNameError("Last Name must be at least 2 characters!");
-    } else {
-      return setLastNameError("");
-    }
-  }
-
   const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
-
-  const emailValidation = (e) => {
-    setEmail(e.target.value);
-    if (email.length < 2) {
-      return setEmailError("Email must be at least 2 charaters!");
-    } else {
-      return setEmailError("");
-    }
-  }
-
   const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-
-  const passwordValidation = (e) => {
-    setPassword(e.target.value);
-    if (password.length < 2) {
-      setPasswordError("Password must be at leaset 2 characters!");
-    } else {
-        return setPasswordError("");
-    }
-  }
-
   const [confirm, setConfirm] = useState("");
-  const [confirmError, setConfirmError] = useState("");
 
-  const confirmValidation = (e) => {
-    console.log(password)
-    setConfirm(e.target.value);
-    if (password !== confirm) {
-      return setConfirmError("Passwords must match!");
-    } else {
-      return setConfirmError("");
-    }
+  const creatUser = (e) => {
+    e.preventDefault();
+
+    const newUser = {firstName, lastName, email, password, confirm};
+    console.log(newUser);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setConfirm("");
+    
   }
 
 
   return (
     <div className="App">
-      <form>
+      <form onSubmit={ creatUser }>
         <div>
+          {firstName.length < 2 && firstName.length > 0 ? <p><b>First Name must be more than 2 characters!</b></p> : null}
           <label>First Name: </label>
-          <input type="text" onChange={ firstNameValidation } value={firstName} />
-          { firstNameError ? <p><b>{firstNameError}</b></p> : null }
+          <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
         </div>
 
         <div>
+          {lastName.length < 2 && lastName.length > 0 ? <p><b>Last Name must be more than 2 characters!</b></p> : null}
           <label>Last Name: </label>
-          <input type="text" onChange={ lastNameValidation } value={lastName} />
-          { lastNameError ? <p><b>{lastNameError}</b></p> : null }
+          <input type="text" onChange={ (e) => setLastName(e.target.value) } value={lastName} />
         </div>
 
         <div>
+          {email.length < 2 && email.length > 0 ? <p><b>Email must be more than 2 characters!</b></p> : null}
           <label>Email: </label>
-          <input type="text" onChange={ emailValidation } value={email} />
-          { emailError ? <p><b>{emailError}</b></p> : null }
+          <input type="text" onChange={ (e) => setEmail(e.target.value) } value={email} />
         </div>
 
         <div>
+          {password.length < 8 && password.length > 0 ? <p><b>Password must be more than 8 characters!</b></p> : null}
           <label>Password: </label>
-          <input type="password" onChange={ passwordValidation } value={password} />
-          { passwordError ? <p><b>{passwordError}</b></p> : null }
+          <input type="password" onChange={ (e) => setPassword(e.target.value) } value={password} />
         </div>
-
         <div>
-        { confirmError ? <p><b>{confirmError}</b></p> : null }
+          {confirm !== password  ? <p><b>Passwords must match!</b></p> : null}
           <label>Confirm Password: </label>
-          <input type="password" onChange={ confirmValidation } value={confirm} />
+          <input type="password" onChange={(e) => setConfirm(e.target.value)} value={confirm} />
         </div>
-
+        <button>Submit</button>
       </form>
     </div>
   );
