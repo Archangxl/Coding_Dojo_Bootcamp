e567db8fd2b419f35d0cd50cd742949751832524
Alexander <lixo.mueller@gmail.com>
2023-04-05 17:24:50 -0600

    last updated 04.05.2023

--- Back_end/Mern/React/postman-pokemon/src/App.js
+++ Back_end/Mern/React/postman-pokemon/src/App.js
@@ -9,12 +9,14 @@ import
 from 'react-router-dom';
 
 import PokemonApi from './Components/PokemonApi';
+import Home from './Components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/pokemon/:numberOrName" element={<PokemonApi />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Back_end/Mern/React/postman-pokemon/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="flex">
+            <p>Hello, in the url put "/pokemon/any pokemon or number"</p>
+        </div>
+    );
+}
+export default Home;
