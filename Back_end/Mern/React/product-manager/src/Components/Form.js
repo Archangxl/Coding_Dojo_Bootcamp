baaa3d4125ec377fb74bcde9b62930137cc4a44a
Alexander <lixo.mueller@gmail.com>
2023-04-10 22:20:06 -0600

    last updated 04.10.2023

--- Back_end/Mern/React/product-manager/src/Components/Form.js
+++ Back_end/Mern/React/product-manager/src/Components/Form.js
@@ -1,22 +1,41 @@
-import react from 'react';
+import react, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-const Form = ( ) => {
+const Form = ( ) => {   
+
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState();
+    const [description, setDescription] = useState("");
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/products', {
+            title,
+            price,
+            description
+        })
+            .then(res=>{
+                console.log(res)
+                console.log(res.data);
+            })
+            .catch(err=>console.log(err));
+    }
 
     return (
         <>
-            <form className='form'>
+            <form onSubmit={onSubmitHandler} className='form'>
                 <p>Product Manager</p>
                 <div className='box grid-1'>
                     <label>Title</label>
-                    <input type="text"></input>
+                    <input onChange={(e)=> setTitle(e.target.value)} type="text"></input>
                 </div>
                 <div className='box grid-2'>
                     <label>Price</label>
-                    <input type="text"></input>
+                    <input onChange={(e)=> setPrice(e.target.value)} type="text"></input>
                 </div>
                 <div className='box grid-3'>
                     <label>Description</label>
-                    <textarea></textarea>
+                    <textarea onChange={(e)=> setDescription(e.target.value)}></textarea>
                 </div>
                 <div className='box grid-4'>
                     <button>Create</button>
